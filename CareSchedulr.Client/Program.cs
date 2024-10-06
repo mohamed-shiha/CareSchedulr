c23e742f75d2ad1a9729d180125a7bfd6d1fc87a
Mohamed Shiha <mohamedshiha89@gmail.com>
2024-10-07 00:24:31 +0100

    fixed login to the server side

--- CareSchedulr.Client/Program.cs
+++ CareSchedulr.Client/Program.cs
@@ -10,13 +10,14 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddMudServices();
+builder.Services.AddScoped<AuthService>();
+builder.Services.AddApiAuthorization();
+builder.Services.AddScoped<AuthenticationService>();
 
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
 
 builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
 builder.Services.AddSingleton<AppointmentService>();
-//builder.Services.AddSingleton<AuthService>();
-builder.Services.AddScoped<AuthService>();
 
 await builder.Build().RunAsync();
---
+++ CareSchedulr.Client/Services/AuthenticationService.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http.Json;
+
+namespace CareSchedulr.Client.Services
+{
+    public class AuthenticationService
+    {
+        private readonly HttpClient _httpClient;
+
+        public AuthenticationService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<bool> LoginAsync(string username, string password)
+        {
+            var response = await _httpClient.PostAsJsonAsync("api/auth/login", new { username, password });
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task LogoutAsync()
+        {
+            await _httpClient.PostAsync("api/auth/logout", null);
+        }
+    }
+
+}
