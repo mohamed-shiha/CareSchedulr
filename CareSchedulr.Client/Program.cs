a41cbf1b296d6789baab39296161dda91b93e587
Mohamed Shiha <mohamedshiha89@gmail.com>
2024-10-07 00:37:57 +0100

    removed added code for custom auth

--- CareSchedulr.Client/Program.cs
+++ CareSchedulr.Client/Program.cs
@@ -10,9 +10,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddMudServices();
-builder.Services.AddScoped<AuthService>();
-builder.Services.AddApiAuthorization();
-builder.Services.AddScoped<AuthenticationService>();
+
 
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
--- CareSchedulr/Data/ApplicationUser.cs
+++ CareSchedulr/Data/ApplicationUser.cs
@@ -5,5 +5,9 @@ namespace CareSchedulr.Data;
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser
 {
+    // should only be 9 chars 
+    public string PPSNumber { get; set; }
+    // only date 
+    public DateTime DOB { get; set; }
 }
 
